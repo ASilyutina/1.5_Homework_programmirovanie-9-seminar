6be9c7958eca81f2a0cd1b3e3625b273c1dbd729
Anastasia Silyutina <ASilyutina@slb.com>
2022-11-13 19:36:18 +0300

    task9.1

---
+++ 9.1_chislaM-N/Program.cs
@@ -0,0 +1,15 @@
+﻿// 9.1 Показать натуральные числа от M до N, N и M заданы
+
+
+string number(int N, int M)
+{
+    if (N>=M) return number(N -1, M) + $"{N} ";
+    else return " ";
+}
+ 
+
+Console.WriteLine(" Input M ");
+int M = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine(" Input  N ");
+int N = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine($"Natural numbers: {number(N,M)}");
