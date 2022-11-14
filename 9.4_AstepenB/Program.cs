cd26a25c14ea95f13668d484452612a4a58aa636
Anastasia Silyutina <ASilyutina@slb.com>
2022-11-14 20:09:47 +0300

    task4

---
+++ 9.4_AstepenB/Program.cs
@@ -0,0 +1,18 @@
+﻿// 9.4 Написать программу возведения числа А в целую стень B
+
+int Power(int A, int B)
+{
+    if (B == 0) return 1;
+    else return Power(A, B - 1) * A;
+}
+
+int ReadIntegerNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int A = ReadIntegerNumber("Input A: ");
+int B = ReadIntegerNumber("exponent for raising a number A: ");
+
+Console.WriteLine(Power(A,B));
