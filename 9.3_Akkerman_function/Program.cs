d52e7a8069d46ac28bf2e22f9cceecc2dcc91b47
Anastasia Silyutina <ASilyutina@slb.com>
2022-11-14 20:02:12 +0300

    task3

---
+++ 9.3_Akkerman_function/Program.cs
@@ -0,0 +1,21 @@
+﻿// 9.3 Написать программу вычисления функции Аккермана
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = InputInt("Input M: ");
+int n = InputInt("Input N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
